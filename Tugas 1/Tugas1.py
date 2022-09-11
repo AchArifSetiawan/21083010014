f56c609346c6aa15b1ef4dc6300f0ce541b87d8f
AchArifSetiawan <21083010014@student.upnjatim.ac.id>
2022-09-11 09:06:01 +0700

    Tugas 1

---
+++ Tugas 1/Tugas1.py
@@ -0,0 +1,10 @@
+garis='-'*40
+print(garis)
+print('Tugas 1 Sistem Operasi Kelas B')
+print(garis)
+print('Nama : Ach. Arif Setiawan')
+print('Npm  : 21083010014')
+print(garis)
+print('Halo Dunia!')
+print('Ini Adalah Skrip Python Pertamaku Di Linux!')
+
---
+++ Tugas 1/Tugas1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Nama : Ach Arif Setiawan
+#Npm  : 21083010014
+
+echo 'Hallo Dunia!'
+echo 'Ini Adalah Skrip Bash Pertamaku Di Linux!'
